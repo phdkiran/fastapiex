e6cc8a60ad603d75cf493c4cfdb3eebe74ce8c6e
Kiran Ramineni <kiran@alumni.uci.edu>
2021-05-01 00:46:53 -0700

    updates

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # fastapiex
+Install packages using requirements.txt
+
+Run application using uvicorn main:app 
+
+Generate transactions using POST /items/ 
+and GET /items/blog_id.
+
+POST  example:
+POST http://localhost:8000/items/
+{"id":2,"author":"kiran","title":"elk","content":"super","date":"2021-01-10"}
+
+GET example:
+http://localhost:8000/items/2
+{"id":2,"author":"kiran","title":"elk","content":"super","date":"2021-01-10"}
+
+http://localhost:8000/items/3
+"Blog id 3 not present"
+
+
+
+http POST localhost:8000/items/ id=2  author=kiran title=elk content=super date=2021-01-10
---
+++ main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from datetime import date
+
+
+class Item(BaseModel):
+    id: int
+    author: str
+    title: str
+    content: str
+    date:date
+
+app = FastAPI()
+
+d = {}
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{blog_id}")
+async def get_by_id(blog_id:int):
+    print(d)
+    res = d[blog_id] if blog_id in d else f'Blog id {blog_id} not present'
+    json_compatible_item_data = jsonable_encoder(res)
+    return JSONResponse(content=json_compatible_item_data)
+
+@app.put("/items/{id}")
+def update_item(id: str, item: Item):
+    json_compatible_item_data = jsonable_encoder(item)
+    return JSONResponse(content=json_compatible_item_data)
+
+@app.post("/items/")
+async def post_item(item: Item):
+    d[item.id] = item
+    return JSONResponse(jsonable_encoder(item))
+
+
